16867f9f3f9ba15047aa189b753d091523f48571
s0hyuneee <s09glee@gmail.com>
2022-05-17 20:15:21 +0900

    한코딩 클론 코딩 시작

---
+++ src/main/java/com/clonehancoding/clone_hancoding/CloneHancodingApplication.java
@@ -0,0 +1,13 @@
+package com.clonehancoding.clone_hancoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneHancodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneHancodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clonehancoding/clone_hancoding/CloneHancodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonehancoding.clone_hancoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneHancodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
